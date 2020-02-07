21abbe22c57621fe4625f91a86fade66dc8dd41a
Lekha <47202180+lekhav@users.noreply.github.com>
2020-02-06 23:37:03 -0800

    Update CardList.js

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-    if (true) {
-        throw new Error('NOOOOOOOOOO');
-    }
+//     if (true) {
+//         throw new Error('NOOOOOOOOOO');
+//     }
     return(
         <div>
             {
